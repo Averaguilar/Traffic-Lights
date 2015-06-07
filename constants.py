e36081e215b3bc2e058c5d4efc5cfa5898e23432
dmckinnon <d_mcchicken@hotmail.com>
2015-06-07 20:16:57 +1000

    Checking a spot for images, updates to viewer road rendering to take this into account

--- constants.py
+++ constants.py
@@ -1,3 +1,18 @@
 """A module for storing constants in the traffic light simulator."""
 
 ROAD_LENGTH = 101
+
+SCREEN_WIDTH = 510
+SCREEN_HEIGHT = 510
+
+# roads are 5 pixels wide, and the length of the screen
+# Each spot in a road is 5 pixels by 5 pixels. 
+ROAD_NORTH_TOP_X = 250
+ROAD_NORTH_TOP_Y = 0
+ROAD_NORTH_BOT_X = 255
+ROAD_NORTH_BOT_Y = 510
+
+ROAD_EAST_TOP_X = 250
+ROAD_EAST_TOP_Y = 0
+ROAD_EAST_BOT_X = 255
+ROAD_EAST_BOT_Y = 510
--- viewer.py
+++ viewer.py
@@ -2,52 +2,54 @@ import pygame
 #import car
 #import road
 #import TrafficLight
-
-SCREEN_WIDTH = 510
-SCREEN_HEIGHT = 510
-
-# roads are 5 pixels wide, and the length of the screen
-ROAD_NORTH_TOP_X = 250
-ROAD_NORTH_TOP_Y = 0
-ROAD_NORTH_BOT_X = 255
-ROAD_NORTH_BOT_Y = 510
-
-ROAD_EAST_TOP_X = 250
-ROAD_EAST_TOP_Y = 0
-ROAD_EAST_BOT_X = 255
-ROAD_EAST_BOT_Y = 510
+import constants as c
 
 class Viewer():
 
-	def initialise():
-		pygame.init()
-		self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    def initialise(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
 
-		# initialise background to grey
-		self.screen.fill(150,150,150)
+        # initialise background to grey
+        self.screen.fill(150,150,150)
 
-		return self.screen
+        return self.screen
 
-	def updateRoads(road1, road2):
-		color = pygame.Color(0, 255, 255, 255)
-		# draw north road. We demand that road1 be the north road
-		pygame.draw.polygon(self.screen, color, [(ROAD_NORTH_TOP_X, ROAD_NORTH_TOP_Y),(ROAD_NORTH_BOT_X, ROAD_NORTH_TOP_Y),(ROAD_NORTH_BOT_X, ROAD_NORTH_BOT_Y),(ROAD_NORTH_TOP_X, ROAD_NORTH_BOT_Y),(ROAD_NORTH_TOP_X, ROAD_NORTH_TOP_Y)], width=0)
+    def update_roads(self, road1, road2):
+        color = pygame.Color(0, 255, 255, 255)
+        # draw north road. We demand that road1 be the north road
+        pygame.draw.polygon(self.screen, color, 
+                [(c.ROAD_NORTH_TOP_X, c.ROAD_NORTH_TOP_Y), 
+                (c.ROAD_NORTH_BOT_X, c.ROAD_NORTH_TOP_Y), 
+                (c.ROAD_NORTH_BOT_X, c.ROAD_NORTH_BOT_Y), 
+                (c.ROAD_NORTH_TOP_X, c.ROAD_NORTH_BOT_Y), 
+                (c.ROAD_NORTH_TOP_X, c.ROAD_NORTH_TOP_Y)], width=0)
 
-		# update that road
-		__updateRoad(road1)
+        # update that road
+        __update_road(road1)
 
-		# draw east road
-		pygame.draw.polygon(self.screen, color, [(ROAD_EAST_TOP_X, ROAD_EAST_TOP_Y),(ROAD_EAST_BOT_X, ROAD_EAST_TOP_Y),(ROAD_EAST_BOT_X, ROAD_EAST_BOT_Y),(ROAD_EAST_TOP_X, ROAD_EAST_BOT_Y),(ROAD_EAST_TOP_X, ROAD_EAST_TOP_Y)], width=0)
+        # draw east road
+        pygame.draw.polygon(self.screen, color, 
+                [(c.ROAD_EAST_TOP_X, c.ROAD_EAST_TOP_Y), 
+                (c.ROAD_EAST_BOT_X, c.ROAD_EAST_TOP_Y), 
+                (c.ROAD_EAST_BOT_X, c.ROAD_EAST_BOT_Y), 
+                (c.ROAD_EAST_TOP_X, c.ROAD_EAST_BOT_Y), 
+                (c.ROAD_EAST_TOP_X, c.ROAD_EAST_TOP_Y)], width=0)
 
-		# update that road
-		__updateRoad(road2)
+        # update that road
+        __update_road(road2)
 
-		# update display changes
-		pygame.display.flip()
+        # update display changes
+        pygame.display.flip()
 
-	def __updateRoad(road):
-		# draw anything on the road
-		for o in road:
-			x,y = o.getLocation()
-			image = o.getImage()
-			self.screen.blit(image, (x,y))
+    def __update_road(self, road):
+        # draw anything on the road
+        for o in road:
+            x,y = o.get_location()
+            images = o.get_images()
+            if len(images) > 1:
+                if len(images) != 0 or images[0] != None:
+                    #TODO(Dave): Modify coords to render car and lights in same box
+                    self.screen.blit(images[0], (x,y)) 
+            if len(images) == 2:
+                    self.screen.blit(images[1], (x,y))

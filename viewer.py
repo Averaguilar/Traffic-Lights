7fc943443c4ffe330dd413f0996106d51c3cd46d
dmckinnon <d_mcchicken@hotmail.com>
2015-06-07 20:40:21 +1000

    Updated road rendering for new spot class

--- viewer.py
+++ viewer.py
@@ -26,7 +26,7 @@ class Viewer():
                 (c.ROAD_NORTH_TOP_X, c.ROAD_NORTH_TOP_Y)], width=0)
 
         # update that road
-        __update_road(road1)
+        __update_north_road(road1)
 
         # draw east road
         pygame.draw.polygon(self.screen, color, 
@@ -37,19 +37,45 @@ class Viewer():
                 (c.ROAD_EAST_TOP_X, c.ROAD_EAST_TOP_Y)], width=0)
 
         # update that road
-        __update_road(road2)
+        __update_east_road(road2)
 
         # update display changes
         pygame.display.flip()
 
-    def __update_road(self, road):
+    def __update_north_road(self, road):
+        y = c.ROAD_NORTH_TOP_Y 
+        x = c.ROAD_NORTH_TOP_X
         # draw anything on the road
-        for o in road:
-            x,y = o.get_location()
+        i = 0
+        while i < c.ROAD_LENGTH:
+            # get 
+            y = (c.ROAD_LENGTH / c.SPOT_SIZE)
+            
             images = o.get_images()
-            if len(images) > 1:
-                if len(images) != 0 or images[0] != None:
+            if len(images) != 0:
+                if images[0] != None:
                     #TODO(Dave): Modify coords to render car and lights in same box
-                    self.screen.blit(images[0], (x,y)) 
-            if len(images) == 2:
+                    self.screen.blit(images[0], (x+2,y+3)) 
+                if len(images) == 2:
                     self.screen.blit(images[1], (x,y))
+
+            i += c.SPOT_SIZE
+
+    def __update_east_road(self, road):
+        y = c.ROAD_EAST_TOP_Y 
+        x = c.ROAD_EAST_TOP_X
+        # draw anything on the road
+        i = 0
+        while i < c.ROAD_LENGTH:
+            # get 
+            x = (c.ROAD_LENGTH / c.SPOT_SIZE)
+            
+            images = o.get_images()
+            if len(images) != 0:
+                if images[0] != None:
+                    #TODO(Dave): Modify coords to render car and lights in same box
+                    self.screen.blit(images[0], (x+3,y+1)) 
+                if len(images) == 2:
+                    self.screen.blit(images[1], (x,y+3))
+
+            i += c.SPOT_SIZE

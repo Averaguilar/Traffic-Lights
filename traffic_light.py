ce7fe2d1cc5f63a25673de115118efd1686b01a6
karl-krauth <karl.krauth@gmail.com>
2015-06-07 18:26:08 +1000

    traffic light class

--- traffic_light.py
+++ traffic_light.py
@@ -1,16 +1,22 @@
-class Color:
-    red = 1
-    green = 2
+"""This module defines the class representing a traffic light."""
 
-class TrafficLight():
+class TrafficLight(object):
+    """This class represents a traffic light that can change state."""
+    RED = 1
+    GREEN = 2
+
     def __init__(self, color):
+        """Creates a new traffic light object that is initially set to color."""
+        assert color == TrafficLight.RED or color == TrafficLight.GREEN
         self._color = color
 
     def flip_color(self):
-        if self._color == red:
-            self._color = green
-        elif self._color == green
-            self._color = red
+        """Change the color of the traffic light."""
+        if self._color == TrafficLight.RED:
+            self._color = TrafficLight.GREEN
+        elif self._color == TrafficLight.GREEN:
+            self._color = TrafficLight.RED
 
     def get_color(self):
+        """Get the current state of the traffic light."""
         return self._color

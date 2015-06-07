76efe43e336ed443209ce36bf9fa9117f9f15e0c
karl-krauth <karl.krauth@gmail.com>
2015-06-07 17:01:59 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+"""This module contains the entry function for the traffic simulator.
+
+Reads argument information and executes a simulation depending on the given
+arguments.
+
+"""
+
+import argparse
+import pygame
+
+
+
+def main():
+    """Entry function for the traffic simulator."""
+    args = parse_args()
+    pygame.init()
+    print args
+
+def parse_args():
+    """Read in commandline arguments and return them in an argument object."""
+    parser = argparse.ArgumentParser(description="A traffic intersection "
+                                     "simulator with various learning "
+                                     "algorithms.")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    main()
